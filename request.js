24f22cbfc8706976e53e63de4f68d684806a9d0e
samsonemeje <samsonemeje@gmail.com>
2021-01-14 18:18:29 +0100

    fix api url

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 const key = 'f341c5aa8a6091cd451a8525cc820c84';
 
 const requestCity = async (city) => {
-	const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+	const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 	const query = `?q=${city}&appid=${key}`;
 
 	// make fetch call(promise call)
